2715d33ffeba9c910051d3eb28be9a0293423943
yannb-dev <yannblondeaudev@gmail.com>
2026-06-27 15:09:28 +0200

    fix(api)transaction roadmap

--- learning-app/app/(app)/api/roadmap/route.ts
+++ learning-app/app/(app)/api/roadmap/route.ts
@@ -37,68 +37,70 @@ export async function POST(request: Request) {
     if (!project)
       return Response.json({ error: "Project introuvable" }, { status: 404 });
 
-    const roadmap = await prisma.roadmap.create({
-      data: {
-        name: result.data.roadmap.name,
-        objective: result.data.roadmap.objective,
-        echeance: date,
-        dispo: result.data.roadmap.dispo,
-        constraint: result.data.roadmap.constraint,
-        duration: result.data.roadmap.duration,
-        userId: session.user.id,
-        projectId: result.data.projectId,
-      },
+    const sendData = await prisma.$transaction(async (tx) => {
+      const roadmap = await tx.roadmap.create({
+        data: {
+          name: result.data.roadmap.name,
+          objective: result.data.roadmap.objective,
+          echeance: date,
+          dispo: result.data.roadmap.dispo,
+          constraint: result.data.roadmap.constraint,
+          duration: result.data.roadmap.duration,
+          userId: session.user.id,
+          projectId: result.data.projectId,
+        },
+      });
+
+      await Promise.all(
+        moduleList.map(async (liste) => {
+          const module = await tx.module.create({
+            data: {
+              name: liste.name,
+              numModule: liste.numModule,
+              duration: liste.duration,
+              prerequisites: liste.prerequisites,
+              pointcritical: liste.pointcritical,
+              practicalproject: liste.practicalproject,
+              roadmapId: roadmap.id,
+            },
+          });
+
+          const newTabCompetence = competenceList.filter(
+            (e) => e.moduleRef === module.numModule,
+          );
+          const newTabCriteria = criteriaList.filter(
+            (e) => e.moduleRef === module.numModule,
+          );
+          await Promise.all([
+            ...newTabCompetence.map((liste) => {
+              return tx.objective.create({
+                data: {
+                  name: liste.name,
+                  index: liste.index,
+                  state: "UpComming",
+                  moduleRef: liste.moduleRef,
+                  moduleId: module.id,
+                  projectId: result.data.projectId,
+                },
+              });
+            }),
+
+            ...newTabCriteria.map((liste) => {
+              return prisma.criteria.create({
+                data: {
+                  name: liste.name,
+                  index: liste.index,
+                  moduleRef: liste.moduleRef,
+                  moduleId: module.id,
+                },
+              });
+            }),
+          ]);
+        }),
+      );
     });
 
-    await Promise.all(
-      moduleList.map(async (liste) => {
-        const module = await prisma.module.create({
-          data: {
-            name: liste.name,
-            numModule: liste.numModule,
-            duration: liste.duration,
-            prerequisites: liste.prerequisites,
-            pointcritical: liste.pointcritical,
-            practicalproject: liste.practicalproject,
-            roadmapId: roadmap.id,
-          },
-        });
-
-        const newTabCompetence = competenceList.filter(
-          (e) => e.moduleRef === module.numModule,
-        );
-        const newTabCriteria = criteriaList.filter(
-          (e) => e.moduleRef === module.numModule,
-        );
-        await Promise.all([
-          ...newTabCompetence.map((liste) => {
-            return prisma.objective.create({
-              data: {
-                name: liste.name,
-                index: liste.index,
-                state: "UpComming",
-                moduleRef: liste.moduleRef,
-                moduleId: module.id,
-                projectId: result.data.projectId,
-              },
-            });
-          }),
-
-          ...newTabCriteria.map((liste) => {
-            return prisma.criteria.create({
-              data: {
-                name: liste.name,
-                index: liste.index,
-                moduleRef: liste.moduleRef,
-                moduleId: module.id,
-              },
-            });
-          }),
-        ]);
-      }),
-    );
-
-    return Response.json({ success: true, data: roadmap });
+    return Response.json({ success: true });
   } catch (err) {
     console.error("Erreur POST /api/roadmap", err);
     return Response.json({ error: "Erreur serveur du POST" }, { status: 500 });
