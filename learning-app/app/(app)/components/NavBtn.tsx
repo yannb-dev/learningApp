e54d5212e0b2ef7d3d4e263b0e96c82d2ab48b84
yannb-dev <yannblondeaudev@gmail.com>
2026-06-29 21:36:35 +0200

    feat(NavBtn) création dyna btn

--- learning-app/app/(app)/components/NavBtn.tsx
+++ learning-app/app/(app)/components/NavBtn.tsx
@@ -6,6 +6,15 @@ import { useState } from "react";
 export default function NavBtn({ idProject }: { idProject: string }) {
   const router = useRouter();
 
+  const titleBtnApp = [
+    { name: "Vue d'ensemble", slug: "accueil" },
+    { name: "Gestion", slug: "gestion" },
+    { name: "Roadmap", slug: "roadmap" },
+    { name: "Memo", slug: "memo" },
+  ];
+
+  const titleBtnCompte = [{ name: "Profil", slug: "profil" }];
+
   const [stateBtn, setStateBtn] = useState("");
 
   const handleNavigation = (page: string) => {
@@ -16,37 +25,25 @@ export default function NavBtn({ idProject }: { idProject: string }) {
   return (
     <div className="w-full flex flex-col items-start mb-12">
       <h3 className="text-gray-400 font-mono text-xl ml-6 mb-6">Application</h3>
-      <button
-        className={`w-full text-start rounded-sm font-mono text-xl mb-2 p-2 hover:bg-gray-200 hover:text-black ${stateBtn === "accueil" ? "bg-gray-200" : ""}`}
-        onClick={() => handleNavigation("accueil")}
-      >
-        Vue d'ensemble
-      </button>
-      <button
-        className={`w-full text-start rounded-sm font-mono text-xl mb-2 p-2 hover:bg-gray-200 hover:text-black ${stateBtn === "gestion" ? "bg-gray-200" : ""}`}
-        onClick={() => handleNavigation("gestion")}
-      >
-        Gestion
-      </button>
-      <button
-        className={`w-full text-start rounded-sm font-mono text-xl mb-2 p-2 hover:bg-gray-200 hover:text-black ${stateBtn === "roadmap" ? "bg-gray-200" : ""}`}
-        onClick={() => handleNavigation("roadmap")}
-      >
-        Roadmap
-      </button>
-      <button
-        className={`w-full text-start rounded-sm font-mono text-xl mb-2 p-2 hover:bg-gray-200 hover:text-black ${stateBtn === "memo" ? "bg-gray-200" : ""}`}
-        onClick={() => handleNavigation("memo")}
-      >
-        Memo
-      </button>
+      {titleBtnApp.map((btn) => (
+        <button
+          key={btn.name}
+          className={`w-full text-start rounded-sm font-mono text-xl mb-2 p-2 hover:bg-gray-200 hover:text-black ${stateBtn === btn.slug ? "bg-gray-200" : ""}`}
+          onClick={() => handleNavigation(btn.slug)}
+        >
+          {btn.name}
+        </button>
+      ))}
       <h3 className="text-gray-400 font-mono text-xl ml-6 mb-6">Compte</h3>
-      <button
-        className={`w-full text-start rounded-sm font-mono text-xl mb-2 p-2 hover:bg-gray-200 hover:text-black ${stateBtn === "profil" ? "bg-gray-200" : ""}`}
-        onClick={() => handleNavigation("profil")}
-      >
-        Profil
-      </button>
+      {titleBtnCompte.map((btn) => (
+        <button
+          key={btn.name}
+          className={`w-full text-start rounded-sm font-mono text-xl mb-2 p-2 hover:bg-gray-200 hover:text-black ${stateBtn === btn.slug ? "bg-gray-200" : ""}`}
+          onClick={() => handleNavigation(btn.slug)}
+        >
+          {btn.name}
+        </button>
+      ))}
     </div>
   );
 }
