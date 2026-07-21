9cb924a5a99c10101ce99c66cf5add4dea3790e0
yannb-dev <yannblondeaudev@gmail.com>
2026-07-21 18:48:55 +0200

    commit before deploy

--- learning-app/lib/auth.ts
+++ learning-app/lib/auth.ts
@@ -14,7 +14,6 @@ export const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
-
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
